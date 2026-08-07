6bd8ec86abffac0114be07181f1a2bea6b84cce5
GUSTAVO RODRIGUES GUIDOLIM PINHO <gustavo.pinho5@senaisp.edu.br>
2026-08-07 11:08:02 -0300

    11:07

--- Pend-master/Javascript/Orientação-a-Objeto/exercicios2.js
+++ Pend-master/Javascript/Orientação-a-Objeto/exercicios2.js
@@ -22,6 +22,28 @@ class Aluno {
         console.log(`Olá, meu nome é ${this.nome}, tenho ${this.idade} anos e estou matriculado no curso de ${this.curso} com a matrícula ${this.matricula}.`);
     }
 }
+console.log("Exercício 2 - Classes e Objetos");
+console.log("--------------------------------");
+console.log("Criando objetos Aluno:");
+console.log("--------------------------------");
+console.log("Aluno 1:");
+console.log("Nome:", "Gustavo");
+console.log("Idade:", 18);
+console.log("Curso:", "Engenharia de Software");
+console.log("Matrícula:", "123456");
+console.log("--------------------------------");
+console.log("Aluno 2:");
+console.log("Nome:", "Henrique");
+console.log("Idade:", 18);
+console.log("Curso:", "Engenharia de Software");
+console.log("Matrícula:", "654321");
+console.log("--------------------------------");
+console.log("Aluno 3:");
+console.log("Nome:", "Victor");
+console.log("Idade:", 18);
+console.log("Curso:", "Engenharia de Software");
+console.log("Matrícula:", "987654");
+console.log("--------------------------------");
 const aluno1 = new Aluno("Gustavo", 18, "Engenharia de Software", "123456");
 const aluno2 = new Aluno("Henrique", 18, "Engenharia de Software", "654321");
 const aluno3 = new Aluno("Victor", 18, "Engenharia de Software", "987654");
